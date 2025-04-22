f700ee66927351c5833667c05fdd7a7c5eeb8640
Anil <anil@test.com>
2025-04-22 14:23:35 +0530

    a

--- user.sh
+++ user.sh
@@ -2,8 +2,9 @@ dnf module disable nodejs -y
 dnf module enable nodejs:20 -y
 dnf install nodejs -y
 
-cp user.service /etc/systemd/system/user.service
 
+cp user.service /etc/systemd/system/user.service
+rm -rf /app
 useradd roboshop
 mkdir /app
 curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user-v3.zip
