3815125cbf00fe82d06f881ac420ec4af1041fe4
Anil <anil@test.com>
2025-04-29 21:18:59 +0530

    roboshop

---
+++ common.sh
@@ -0,0 +1,23 @@
+systemd_setup() {
+  systemctl daemon-reload
+  systemctl enable ${component_name}
+  systemctl start ${component_name}
+}
+
+nodejs() {
+  cp ${component_name}.service /etc/systemd/system/${component_name}.service
+  dnf module disable nodejs -y
+  dnf module enable nodejs:20 -y
+  dnf install nodejs -y
+  useradd roboshop
+  rm -rf /app
+  mkdir /app
+  cd /app
+  npm install
+  curl -L -o /tmp/${component_name}.zip https://roboshop-artifacts.s3.amazonaws.com/${component_name}-v3.zip
+  cd /app
+  unzip /tmp/${component_name}.zip
+  cd /app
+  npm install
+}
+
--- user.sh
+++ user.sh
@@ -1,17 +1,30 @@
-dnf module disable nodejs -y
-dnf module enable nodejs:20 -y
-dnf install nodejs -y
+component_name=user
+source common.sh
 
-cp user.service /etc/systemd/system/user.service
-useradd roboshop
 
-mkdir /app
-curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user-v3.zip
-cd /app
-unzip /tmp/user.zip
-cd /app
-npm install
 
-systemctl daemon-reload
-systemctl enable user
-systemctl start user
+#dnf module disable nodejs -y    ---nodejs function
+#dnf module enable nodejs:20 -y
+#dnf install nodejs -y
+
+#cp user.service /etc/systemd/system/user.service
+
+nodejs
+
+# nodejs function
+#useradd roboshop
+#rm -rf /app
+#mkdir /app
+#cd /app
+#npm install
+#curl -L -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart-v3.zip
+#cd /app
+#unzip /tmp/cart.zip
+#cd /app
+#npm install
+
+#systemctl daemon-reload
+#systemctl enable cart
+#systemctl start cart
+
+systemd_setup
